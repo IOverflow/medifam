604c318c1e9fac88de34cc12878da910c9eba6ae
adriangs1996 <adriangonzalezsanchez1996@gmail.com>
2021-02-01 23:56:45 -0500

    Add password length verification test

--- medifam_serv_project/accounts/tests.py
+++ medifam_serv_project/accounts/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.urls.base import reverse
+from django.urls import reverse
 from rest_framework import response, status
 from rest_framework.test import APITestCase
 
@@ -18,7 +18,11 @@ class AccountTest(APITestCase):
 
     def test_create_user(self):
         "Make sure we can create a new user and a valid token is created with it"
-        user_data = {"username": "foo", "email": "foo@example.com", "password": "pass"}
+        user_data = {
+            "username": "foo",
+            "email": "foo@example.com",
+            "password": "*7&12.<>$passAcc",
+        }
 
         response = self.client.post(self.create_url, user_data, format="json")
 
@@ -32,4 +36,26 @@ class AccountTest(APITestCase):
         # NOT THE PASSWORD
         self.assertEqual(response.data["username"], user_data["username"])
         self.assertEqual(response.data["email"], user_data["email"])
-        self.assertFalse("password" in response.data)
+        self.assertFalse("*7&12.<>$passAcc" in response.data)
+
+    # Test for password sanity
+    def test_long_password(self):
+        "Test that passwords cannot contain less than 8 characters."
+        short_user_pass_data = {
+            "username": "foo",
+            "email": "email@example.com",
+            "password": "pass",
+        }
+
+        response = self.client.post(
+            self.create_url,
+            short_user_pass_data,
+            format="json",
+        )
+
+        # Test that we received status code 411 (Short)
+        self.assertEqual(response.status_code, status.HTTP_411_LENGTH_REQUIRED)
+
+    def test_login_user(self):
+        "Make sure we can login an existing user"
+        pass
